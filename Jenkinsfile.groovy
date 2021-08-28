390afcffea6ca7af375770112b0c0018a5b036b4
BitBucket Access Account <bitadmin@poyaskov.ca>
2021-08-28 12:21:31 -0400

    Debugging

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -116,8 +116,8 @@ stage("Kubectl config"){
 
 stage("Deploy MySQL DB"){
     node("${env.NodeName}"){
-    wrap([$class: 'AnsiColorBuildWrapper']){
-       withEnv(['KubeConfigSafe=' + KubeConfigSafe, 'RepoImageName=' + LIMAGE, 'VERSION=' + VERSIONA]){
+    wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']){
+       withEnv(['KubeConfigSafe=' + KubeConfigSafe, 'RepoImageName=' + LIMAGE, 'VERSION=' + VERSION]){
           withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'Jenkins_KubeMaster', usernameVariable: 'UserName', passwordVariable: 'Password']]){
 
              echo '[Pipeline][INFO] Deploy MySQL(MariaDB) to the k8s Cluster...'
